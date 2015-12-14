db69cdf495c7cda010d82afdc9cde01dbee901bd
fabien <fabien.pelisson@innovisite.com>
2015-12-14 13:27:13 +0100

    Rewrite to be stream compatible

--- index.js
+++ index.js
@@ -7,79 +7,62 @@ var gutil = require('gulp-util');
 var through = require('through2');
 var path = require('path');
 var glob = require('glob');
+var PluginError = gutil.PluginError;
 
-var pluginName = require('./package.json').name;
+var PLUGIN_NAME = require('./package.json').name;
 
-module.exports = function(poFiles) {
-    poFiles = glob.sync(poFiles);
+function getParsedPO(filename) {
+    var content = fs.readFileSync(filename);
+    return PO.parse(content.toString());    
+}
 
-    var languages = _.map(poFiles, function(file) {
-	return path.basename(file, '.po')
-    });
-    
-    var parsedPoFiles = _.map(poFiles, function(poFile) {
-	var content = fs.readFileSync(poFile);
-	return PO.parse(content.toString());
-    });
+function gulpTranslate(poFile) {
+    var language = path.basename(poFile, '.po');      
+    var parsedPoFile = getParsedPO(poFile);
+    var items = parsedPoFile.items;
 
-    var translations = _.zipObject(languages, _.pluck(parsedPoFiles, 'items'));
-
-    //var poContent = fs.readFileSync(poFile);
-    //var poParsed = PO.parse(poContent.toString());
-
-    return through.obj(function(file, enc, throughCallback) {
-	if (file.isNull()) {
-	    this.push(file);
-	    throughCallback();
-	    return;
+    return through.obj(function(file, enc, cb) {
+	
+	if(file.isNull()) {
+	    return cb(null, file);
 	}
 
-	var self = this;
-	_.forEach(languages, function(language) {
-	    var translation = translations[language];
-	    var newFile = file.clone();
-	    var src = newFile.contents.toString();
-	    var dst = '';
-
-	    var TOKEN_START = "@<<";
-	    var TOKEN_END = ">>@";
-	    var TOKEN_LENGTH = TOKEN_START.length;
-	    var lastIndex = 0;
-	    var finished = false;
-	    while(!finished) {
-		var tokenStart = src.indexOf(TOKEN_START, lastIndex);
-		if(tokenStart != -1) {
-		    dst += src.substring(lastIndex, tokenStart);
-		    var tokenEnd = src.indexOf(TOKEN_END, tokenStart);
-		    if(tokenEnd != -1) {
-			var trStr = src.substring(tokenStart + TOKEN_LENGTH,
-						  tokenEnd);
-			for(var i in translation) {			    
-			    var trItem = translation[i];
-			    if(trItem.msgid === trStr) {	    
-				trStr = trItem.msgstr[0];
-				break;
-			    }
+	var src = file.contents.toString();
+	var dst = '';		
+	var TOKEN_START = "@<<";
+	var TOKEN_END = ">>@";
+	var TOKEN_LENGTH = TOKEN_START.length;
+	var lastIndex = 0;
+	var finished = false;
+	while(!finished) {
+	    var tokenStart = src.indexOf(TOKEN_START, lastIndex);
+	    if(tokenStart != -1) {
+		dst += src.substring(lastIndex, tokenStart);
+		var tokenEnd = src.indexOf(TOKEN_END, tokenStart);
+		if(tokenEnd != -1) {
+		    var trStr = src.substring(tokenStart + TOKEN_LENGTH,
+					      tokenEnd);
+		    for(var i in items) {
+			var trItem = items[i];
+			if(trItem.msgid === trStr) {	    
+			    trStr = trItem.msgstr[0];
+			    break;
 			}
-			dst += trStr;
-			lastIndex = tokenEnd + TOKEN_LENGTH;
-		    } else {
-			dst += src.substring(tokenStart);
-			finished = true;
 		    }
+		    dst += trStr;
+		    lastIndex = tokenEnd + TOKEN_LENGTH;
 		} else {
-		    dst += src.substring(lastIndex);
+		    dst += src.substring(tokenStart);
 		    finished = true;
 		}
+	    } else {
+		dst += src.substring(lastIndex);
+		finished = true;
 	    }
-
-	    newFile.contents = new Buffer(dst);
-	    newFile.path = language + "/" + path.basename(newFile.path);
-	    newFile.base = './';
-	    self.push(newFile);
-	});
+	}	
+	file.contents = new Buffer(dst);
+	cb(null, file);
     });
+}
 
-    throughCallback();
-};
-
+module.exports = gulpTranslate;
